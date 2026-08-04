784115fc8f6207332b24a53010c72001955b7b71
raulmasegosa-del <raulmasegosa@gmail.com>
2026-08-04 20:54:04 +0200

    Sprint 19.1 - Acción de cobro de mesa

--- src/components/tables/TableActions.tsx
+++ src/components/tables/TableActions.tsx
@@ -11,37 +11,68 @@ export default function TableActions({
   orderId,
   status,
 }: Props) {
-  if (status !== "preparing") {
-    return null;
-  }
-
-  async function serve() {
+  async function changeStatus(
+    newStatus: TableStatus
+  ) {
     "use server";
 
     await updateTableStatus(
       orderId,
-      "served"
+      newStatus
     );
   }
 
   return (
-    <form action={serve}>
-      <button
-        className="
-          mt-8
-          w-full
-          rounded-xl
-          bg-green-600
-          px-5
-          py-3
-          font-semibold
-          text-white
-          transition
-          hover:bg-green-700
-        "
-      >
-        🟢 Servir pedido
-      </button>
-    </form>
+    <div className="mt-8 space-y-3">
+      {status === "preparing" && (
+        <form
+          action={changeStatus.bind(
+            null,
+            "served"
+          )}
+        >
+          <button
+            className="
+              w-full
+              rounded-xl
+              bg-green-600
+              px-5
+              py-3
+              font-semibold
+              text-white
+              transition
+              hover:bg-green-700
+            "
+          >
+            🟢 Servir pedido
+          </button>
+        </form>
+      )}
+
+      {status === "bill" && (
+        <form
+          action={changeStatus.bind(
+            null,
+            "completed"
+          )}
+        >
+          <button
+            className="
+              w-full
+              rounded-xl
+              bg-emerald-600
+              px-5
+              py-3
+              font-semibold
+              text-white
+              transition
+              hover:bg-emerald-700
+            "
+          >
+            💶 Cobrar mesa
+          </button>
+        </form>
+      )}
+    </div>
   );
 }
