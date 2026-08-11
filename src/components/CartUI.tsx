132559bf00cb0bb45faee7e54a775a649c69bcaf
raulmasegosa-del <raulmasegosa@gmail.com>
2026-08-11 07:37:04 +0200

    Allow customers to continue ordering after first order

--- src/components/CartUI.tsx
+++ src/components/CartUI.tsx
@@ -5,8 +5,6 @@ import { useState } from "react";
 import Cart from "./cart/Cart";
 import CartButton from "./CartButton";
 
-import { useOrder } from "@/context/OrderContext";
-
 type Props = {
   restaurantId: string;
 };
@@ -14,19 +12,10 @@ type Props = {
 export default function CartUI({ restaurantId }: Props) {
   const [open, setOpen] = useState(false);
 
-  const { order } = useOrder();
-
-  function handleOpen() {
-    if (order) return;
-
-    setOpen(true);
-  }
-
   return (
     <>
       <CartButton
-        onClick={handleOpen}
-        disabled={!!order}
+        onClick={() => setOpen(true)}
       />
 
       <Cart
